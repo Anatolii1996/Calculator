ea0f989283fea681623d35a2597bd97cde81db34
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2022-11-16 12:26:49 +0200

    добавил математику

---
+++ main.js
@@ -0,0 +1,27 @@
+let result = document.getElementById('result');
+let res = 0;
+
+function calc() {
+    let arg1 = Number(document.getElementById('first-arg').value);
+    let arg2 = Number(document.getElementById("second-arg").value);
+    let action = String(document.getElementById("math").value);
+
+    switch (action) {
+        case "+":
+            res = arg1 + arg2;
+            break;
+        case "-":
+            res = arg1 - arg2;
+            break;
+        case "*":
+            res = arg1 * arg2;
+            break;
+        case "/":
+            res = arg1 / arg2;
+            break;
+
+    }
+
+    result.innerHTML = `${res} `;
+}
+
