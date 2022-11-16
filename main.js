4fb291ced956cbccfc744fca33bf534bac0a6a9c
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2022-11-16 13:13:10 +0200

    добавил callback функции в calc

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let result = document.getElementById('result');
 let res = 0;
 
+const add = (x, y) => (+x) + (+y);
+const subtract = (x, y) => (+x) - (+y);
+const multiply = (x, y) => (+x) * (+y);
+const divide = (x, y) => (+x) / (+y);
+
+
 function calc() {
     let arg1 = Number(document.getElementById('first-arg').value);
     let arg2 = Number(document.getElementById("second-arg").value);
@@ -8,16 +14,16 @@ function calc() {
 
     switch (action) {
         case "+":
-            res = arg1 + arg2;
+            res = add(arg1, arg2);
             break;
         case "-":
-            res = arg1 - arg2;
+            res = subtract(arg1, arg2);
             break;
         case "*":
-            res = arg1 * arg2;
+            res = multiply(arg1, arg2);
             break;
         case "/":
-            res = arg1 / arg2;
+            res = divide(arg1, arg2);
             break;
 
     }
@@ -25,10 +31,6 @@ function calc() {
     result.innerHTML = `${res} `;
 }
 
-const add = (x, y) => (+x) + (+y);
-const subtract = (x, y) => (+x) - (+y);
-const multiply = (x, y) => (+x) * (+y);
-const divide = (x, y) => (+x) / (+y);
 
 module.exports = { calc, add, subtract, multiply, divide };
 
