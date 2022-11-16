411e5daa4236ecb09bc572b862e3a19bcdb51d04
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2022-11-16 17:50:46 +0200

    финальная версия

--- main.js
+++ main.js
@@ -1,36 +1,40 @@
-let result = document.getElementById('result');
-let res = 0;
-
 const add = (x, y) => (+x) + (+y);
 const subtract = (x, y) => (+x) - (+y);
 const multiply = (x, y) => (+x) * (+y);
 const divide = (x, y) => (+x) / (+y);
 
-
-function calc() {
+function collectData() {
     let arg1 = Number(document.getElementById('first-arg').value);
     let arg2 = Number(document.getElementById("second-arg").value);
     let action = String(document.getElementById("math").value);
+    return { arg1, arg2, action }
+}
 
+function showResult(result) {
+    let resultArr = document.getElementById('result');
+    resultArr.innerHTML = `${result} `;
+}
+
+function calc({ arg1, arg2, action }) {
     switch (action) {
         case "+":
-            res = add(arg1, arg2);
-            break;
+            return add(arg1, arg2);
         case "-":
-            res = subtract(arg1, arg2);
-            break;
+            return subtract(arg1, arg2);
         case "*":
-            res = multiply(arg1, arg2);
-            break;
+            return multiply(arg1, arg2);
         case "/":
-            res = divide(arg1, arg2);
-            break;
+            return divide(arg1, arg2);
 
     }
 
-    result.innerHTML = `${res} `;
+}
+function processCalc() {
+    let collectedData = collectData();
+    let calculatedData = calc(collectedData);
+    showResult(calculatedData);
 }
 
 
-module.exports = { calc, add, subtract, multiply, divide };
+module.exports = { calc, add, subtract, multiply, divide, processCalc };
 
--- test/test.js
+++ test/test.js
@@ -1,4 +1,33 @@
+const { calc, add, subtract, multiply, divide } = require("../main");
 const assert = require("assert");
-const main = require("../main");
 
+
+it("проверяет сложение", () => {
+    assert.equal(add(2, 8), 10);
+})
+it("проверяет вычитание", () => {
+    assert.equal(subtract(8, 2), 6);
+})
+it("проверяет умножение", () => {
+    assert.equal(multiply(2, 8), 16);
+})
+it("проверяет деление", () => {
+    assert.equal(divide(8, 4), 2);
+})
+
+describe("calc", () => {
+    it("проверяет калькуляцию", () => {
+        assert.equal(calc({ arg1: 8, arg2: 4, action: "+" }), 12);
+    })
+
+    it("проверяет калькуляцию выч", () => {
+        assert.equal(calc({ arg1: 8, arg2: 4, action: "-" }), 4);
+    })
+    it("проверяет калькуляцию дел", () => {
+        assert.equal(calc({ arg1: 8, arg2: 4, action: "/" }), 2);
+    })
+    it("проверяет калькуляцию умнож", () => {
+        assert.equal(calc({ arg1: 8, arg2: 4, action: "*" }), 32);
+    })
+})
 
